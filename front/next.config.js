20531135480432434c07a9ecb6c3f364eb73e4a5
LeeMineo <lms4882@gmail.com>
2024-07-22 17:10:01 +0900

    Update next.config.js

--- front/next.config.js
+++ front/next.config.js
@@ -10,20 +10,9 @@ const nextConfig = {
     return [
       {
         source: '/products/:id',
-        destination: '/product/:id', // 올바른 목적지 설정
+        destination: '/product/:id',
       },
     ];
-  },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/cart': { page: '/cart' },
-      '/products': { page: '/products' },
-      // 필요한 다른 경로들도 여기에 추가
-    }
   },
 };
 
